5f503bda18a133e8864a03ddf3ec246fa467eb79
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-10-19 23:45:20 +0900

    feat: esm

---
+++ module/ESM/index.js
@@ -0,0 +1,6 @@
+//묘듈 내보내고 가져오기
+import {getCircleArea} from './mathUtil'
+
+const result = getCircleArea(2)
+
+console.log(result)
--- module/commonJs/mathUtil.js
+++ module/commonJs/mathUtil.js
@@ -1,7 +1,7 @@
 const PI = 3.14
 const getCircleArea = r => r*r*PI
 
-// module.exports = {PI,getCircleArea}
+module.exports = {PI,getCircleArea}
 
 exports.PI = PI
 exports.getCircleArea = getCircleArea
